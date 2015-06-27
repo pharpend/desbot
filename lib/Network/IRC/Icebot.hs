9295231915c2ddb4858a626005c88ef07765b66a
Peter Harpending <peter@harpending.org>
2015-06-26 18:26:46 -0600

    Add runServer function to library.

--- lib/Network/IRC/Icebot.hs
+++ lib/Network/IRC/Icebot.hs
@@ -3,22 +3,22 @@
 
 -- icebot - bot for #snowdrift on FreeNode
 -- Copyright (c) 2015, Peter Harpending.
--- 
+--
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU Affero General Public License as
 -- published by the Free Software Foundation, either version 3 of the
 -- License, or (at your option) any later version.
--- 
+--
 -- This program is distributed in the hope that it will be useful, but
 -- WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 -- Affero General Public License for more details.
--- 
+--
 -- You should have received a copy of the GNU Affero General Public
 -- License along with this program.  If not, see
 -- <http://www.gnu.org/licenses/>.
 
--- | 
+-- |
 -- Module      : Network.IRC.Icebot
 -- Description : Umbrella module for the icebot library
 -- Copyright   : Copyright (c) 2015, Peter Harpending.
@@ -27,7 +27,7 @@
 -- Stability   : experimental
 -- Portability : POSIX
 
-module Network.IRC.Icebot 
+module Network.IRC.Icebot
   ( -- ** Convenience re-exports
     module Control.Exceptional
     -- * Configuration
@@ -35,17 +35,22 @@ module Network.IRC.Icebot
   , Server(..)
   , readConfig
   , readConfigFile
+    -- * Running the server
+  , runServer
   )
   where
 
+import Control.Concurrent
 import Control.Exceptional
-import Control.Monad (mzero)
-import Control.Parallel
+import Control.Monad
 import Data.String (IsString(..))
 import Data.Text (Text, pack)
+import qualified Data.Text.IO as T
 import Data.Yaml
 import Network
+import Paths_icebot
 import System.Directory
+import System.Exit
 import System.IO
 
 -- |The Icebot configuration is a list of servers to connect to.
@@ -57,29 +62,35 @@ data Server =
          ,srvUsername :: Text
          ,srvPassword :: Password
          ,srvChannels :: [Text]
+         ,srvId :: Text
          ,srvHostname :: HostName
          ,srvPort :: Integer
+         ,srvLogFile :: Maybe FilePath
          }
   deriving (Eq,Show)
- 
+
 
 instance FromJSON Server where
   parseJSON (Object v) = Server <$> v .: "nick"
                                 <*> v .: "username"
-                                <*> v .: "password"
+                                <*> v .:? "password" .!= NoPassword
                                 <*> v .: "channels"
+                                <*> v .: "id"
                                 <*> v .: "hostname"
                                 <*> v .: "port"
+                                <*> v .: "log-file"
   parseJSON _ = mzero
 
 instance ToJSON Server where
-  toJSON (Server n u p c h o) =
+  toJSON (Server n u p c i h o l) =
     object ["nick" .= n
            ,"username" .= u
            ,"password" .= p
            ,"channels" .= c
+           ,"id" .= i
            ,"hostname" .= h
-           ,"port" .= o]
+           ,"port" .= o
+           ,"log-file" .= l]
 
 -- |A password can either be demanded from stdin, omitted, or encoded
 data Password = PromptPassword
@@ -98,7 +109,7 @@ instance ToJSON Password where
   toJSON PromptPassword = String "prompt"
   toJSON NoPassword = Null
   toJSON (NickServPassword s) = String s
-  
+
 -- | > fromString "prompt" = PromptPassword
 -- > fromString "stdin" = PromptPassword
 -- > fromString "" = NoPassword
@@ -113,11 +124,12 @@ instance IsString Password where
 -- exception-safe, in that all exceptions are caught in the
 -- 'Exceptional' monad, rather than the 'IO' monad.
 readConfigFile :: FilePath -> IO (Exceptional IceConfig)
-readConfigFile fp = do decoded <- exceptIO $ decodeFileEither fp
-                       return $ case decoded of
-                                  Failure x -> Failure x
-                                  Success (Left e) -> Failure $ prettyPrintParseException e
-                                  Success (Right e) -> Success e
+readConfigFile fp =
+  do decoded <- exceptIO $ decodeFileEither fp
+     return $ case decoded of
+                Failure x -> Failure x
+                Success (Left e) -> Failure $ prettyPrintParseException e
+                Success (Right e) -> Success e
 
 -- |Read icebot.yaml from the current directory, try to marshal it into
 -- an 'IceConfig'.
@@ -127,12 +139,41 @@ readConfig = do path <- exceptIO $ makeAbsolute "icebot.yaml"
                   Failure err -> return $ Failure err
                   Success pth -> readConfigFile pth
 
--- |Given an 'IceConfig', connect to each of the servers, join the
--- channels in parallel, return one handle per server.
-runIceConfig :: IceConfig -> IO [Handle]
-runIceConfig [] = return []
-runIceConfig (x:xs) =
-  do handle <- connectTo (srvHostname x)
-                         (PortNumber (fromInteger (srvPort x)))
-     rest <- par handle (runIceConfig xs)
-     return (handle : rest)
+-- |Connect to a server.
+runServer :: Server -> IO ThreadId
+runServer srv =
+  do hdl <-
+       connectTo (srvHostname srv)
+                 (PortNumber (fromInteger (srvPort srv)))
+     hSetBinaryMode hdl True
+     hSetBuffering hdl NoBuffering
+     writeHdl hdl $
+       mappend "NICK " (srvNick srv)
+     writeHdl hdl $
+       mconcat ["USER ",srvNick srv," 0 * :icebot!"]
+     forM_ (srvChannels srv) $
+       \chan ->
+         writeHdl hdl $
+         mappend "JOIN " chan
+     case srvPassword srv of
+       NoPassword -> return ()
+       PromptPassword ->
+         do pwd <- T.getLine
+            writeHdl hdl
+                     (mconcat ["PRIVMSG NickServ :IDENTIFY "
+                              ,srvNick srv
+                              ," "
+                              ,pwd])
+       NickServPassword pwd ->
+         writeHdl hdl
+                  (mconcat ["PRIVMSG NickServ :IDENTIFY ",srvNick srv," ",pwd])
+     forkIO (forever (do hdlOpen <- hIsOpen hdl
+                         unless hdlOpen exitSuccess))
+  where writeHdl hdl txt =
+          do T.hPutStr hdl (mappend txt "\r\n")
+             case srvLogFile srv of
+               Nothing -> return ()
+               Just fp ->
+                 do fp' <- makeAbsolute fp
+                    T.appendFile fp'
+                                 (mappend txt "\n")
